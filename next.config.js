b7198d6fe3b77b25d845775c6d3031fb06ccbaf3
Dewey Bennett <debennett3@gmail.com>
2022-11-07 08:46:48 -0500

    v1.3

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images:{
+    domains: [
+      'cdn.sanity.io',
+      'links.papareact.com',
+      'accountabilitylab.org',
+    ]
+  }
 }
 
 module.exports = nextConfig
